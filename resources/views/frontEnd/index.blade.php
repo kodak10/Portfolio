bd82a4bff7321f08608371e46c1df5a0d6013931
kodak10 <atchinaymard10@gmail.com>
2022-06-21 10:56:03 +0000

    ok

--- resources/views/frontEnd/index.blade.php
+++ resources/views/frontEnd/index.blade.php
@@ -12,7 +12,7 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
     
-    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="asses/css/owl.carousel.min.css">
     <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
 </head>
 <body>
