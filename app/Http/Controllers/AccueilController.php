95abe8bb427963c121aa0522b3021801e8ae2ece
kodak10 <atchinaymard10@gmail.com>
2022-06-24 10:15:22 +0000

    Push

--- app/Http/Controllers/AccueilController.php
+++ app/Http/Controllers/AccueilController.php
@@ -66,6 +66,7 @@ class AccueilController extends Controller
                                             'competence',
                                             'langage',
                                         ));
+                                        
         }
 
     /**
